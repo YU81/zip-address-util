afd5e86e0dead66c75ab48099af11afd6cf7cab2
YU81 <yukiho0818@gmail.com>
2015-11-22 22:37:26 +0900

    Apply Bootstrap on search form.

--- resources/views/index.php
+++ resources/views/index.php
@@ -7,10 +7,6 @@
     <link rel="stylesheet" href="/assets/css/bootstrap-theme.min.css">
 </head>
 <body>
-<h1>Zip Address Util</h1>
-<form name="search_form" method="get" action="/address/result">
-    <button>検索</button>
-</form>
 <div id="result">
     <?php if (isset($results)) {
         foreach ($results as $addr) {
@@ -18,13 +14,24 @@
             echo '<div><span>' . $addr->getWholeAddress() . '</span></div>';
         }
     } ?>
-    <input type="text" name="search" title="search" id="search"
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6 col-lg-offset-3">
+            <h1>Zip Address Util</h1>
+        </div>
+    </div>
+    <div class="row">
+        <form name="search_form" method="get" action="/address/result" class="col-lg-6 col-lg-offset-3">
+            <input type="text" class="input-sm col-lg-6" name="search" title="search" id="search"
                    value=<?php
                    /** @var string $searchWord */
                    if (isset($searchWord)) {
                        echo $searchWord;
                    } ?>
             >
+            <button class="btn btn-success col-lg-2 glyphicon-search">検索</button>
+        </form>
+    </div>
 </div>
 <script src="/assets/js/jquery.min.js"></script>
 </body>
