8ccbd60c168c7b08ff509fffa99a44b31063b61e
YU81 <yukiho0818@gmail.com>
2015-11-22 22:37:53 +0900

    Apply Bootstrap and table on result area.

--- resources/views/index.php
+++ resources/views/index.php
@@ -7,13 +7,6 @@
     <link rel="stylesheet" href="/assets/css/bootstrap-theme.min.css">
 </head>
 <body>
-<div id="result">
-    <?php if (isset($results)) {
-        foreach ($results as $addr) {
-            /** @var App\Models\Address $addr */
-            echo '<div><span>' . $addr->getWholeAddress() . '</span></div>';
-        }
-    } ?>
 <div class="container">
     <div class="row">
         <div class="col-lg-6 col-lg-offset-3">
@@ -32,7 +25,26 @@
             <button class="btn btn-success col-lg-2 glyphicon-search">検索</button>
         </form>
     </div>
+    <div id="result" class="row">
+        <?php
+        /** @var int $count */
+        if ($count === 0) {
+            echo 'マッチする検索結果はありませんでした。';
+        } elseif (isset($results)) {
+            echo '<span>' . $count . '件見つかりました。' . '</span>';
+            echo '<table class="table table-bordered">';
+            echo '<th>郵便番号</th><th>住所表記</th>';
+            foreach ($results as $addr) {
+                /** @var App\Models\Address $addr */
+                echo '<tr>';
+                echo '<td>' . $addr->zip . '</td>' . '<td><span>' . $addr->getWholeAddress() . '</span></td>';
+                echo '</tr>';
+            }
+            echo '</table>';
+        } ?>
+    </div>
 </div>
 <script src="/assets/js/jquery.min.js"></script>
 </body>
+
 </html>
