8965f955c72920247f3600b43c0bf9b1f7118525
YU81 <yukiho0818@gmail.com>
2015-11-23 14:34:54 +0900

    Add test cases for Address Model.

--- tests/AddressTestCase.php
+++ tests/AddressTestCase.php
@@ -9,5 +9,23 @@ class AddressTest extends TestCase
 
         self::assertTrue(count($results) === 1);
     }
+    public function testZipNormalize1()
+    {
+        $codes = ['160-0022', '160ー0022', '160－0022', '160―0022'];
+        foreach ($codes as $zip) {
+            $processed = App\Models\Address::processZip($zip);
+            self::assertTrue($processed === $codes[0]);
+            $processed2 = App\Models\Address::processZip($zip, false);
+            self::assertTrue($processed2 === '1600022');
+        }
+    }
 
+    public function testZipNormalize2()
+    {
+        $zip       = '1600022';
+        $processed = App\Models\Address::processZip($zip);
+        self::assertTrue($processed === '160-0022');
+        $processed2 = App\Models\Address::processZip($zip, false);
+        self::assertTrue($processed2 === $zip);
+    }
 }
